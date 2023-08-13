ada65e7786a5f5914724ab01c4e8751128bb1a0d
Thiago de Assis <pthiagodev@gmail.com>
2023-08-13 17:08:55 -0300

    adiciona config de secret da geração do token

--- src/main/java/pthiagodev/universidadeweb/api/infra/security/TokenService.java
+++ src/main/java/pthiagodev/universidadeweb/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package pthiagodev.universidadeweb.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import pthiagodev.universidadeweb.api.domain.usuario.Usuario;
 
@@ -14,9 +15,12 @@ import java.util.Date;
 @Service
 public class TokenService {
 
+    @Value("${api.security.token.secret}")
+    private String secret;
+
     public String gerarToken(Usuario usuario) {
         try {
-            var algoritmo = Algorithm.HMAC256("123");
+            var algoritmo = Algorithm.HMAC256(secret);
             return JWT.create()
                     .withIssuer("API universidadeweb")
                     .withSubject(usuario.getLogin())
