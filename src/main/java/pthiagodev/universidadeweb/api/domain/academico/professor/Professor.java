7b86074c83880bb926ec407bb2dc2b5e901602e3
Thiago de Assis <pthiagodev@gmail.com>
2023-08-14 00:48:14 -0300

    cria entidade Professor

---
+++ src/main/java/pthiagodev/universidadeweb/api/domain/academico/professor/Professor.java
@@ -0,0 +1,10 @@
+package pthiagodev.universidadeweb.api.domain.academico.professor;
+
+import pthiagodev.universidadeweb.api.domain.academico.disciplina.Disciplina;
+
+
+public class Professor {
+
+    private Long id;
+    private Disciplina disciplina;
+}
