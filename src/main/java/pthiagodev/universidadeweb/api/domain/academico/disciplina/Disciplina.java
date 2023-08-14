c0075e4d4e758aff72a41022d074a105762a4182
Thiago de Assis <pthiagodev@gmail.com>
2023-08-14 00:48:05 -0300

    cria entidade Disciplina

---
+++ src/main/java/pthiagodev/universidadeweb/api/domain/academico/disciplina/Disciplina.java
@@ -0,0 +1,13 @@
+package pthiagodev.universidadeweb.api.domain.academico.disciplina;
+
+import pthiagodev.universidadeweb.api.domain.academico.professor.Professor;
+
+
+public class Disciplina {
+
+    private Long id;
+    private String nome;
+    private int cargaHoraria;
+    private Professor professor;
+
+}
