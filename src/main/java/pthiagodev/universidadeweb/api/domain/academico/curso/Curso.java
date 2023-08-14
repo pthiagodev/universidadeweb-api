e168420f05b44fa6dba10f8458a977cff018e6d6
Thiago de Assis <pthiagodev@gmail.com>
2023-08-14 00:47:27 -0300

    cria entidade Curso

---
+++ src/main/java/pthiagodev/universidadeweb/api/domain/academico/curso/Curso.java
@@ -0,0 +1,12 @@
+package pthiagodev.universidadeweb.api.domain.academico.curso;
+
+import pthiagodev.universidadeweb.api.domain.academico.matrizCurricular.MatrizCurricular;
+
+public class Curso {
+
+    private Long id;
+    private String nome;
+    private String codigo;
+
+    private MatrizCurricular matrizCurricular;
+}
