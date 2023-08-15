fe13f2423609ef598243de02368c0a49e16a3e82
Thiago de Assis <pthiagodev@gmail.com>
2023-08-14 21:50:31 -0300

    implementa repositório de Curso

---
+++ src/main/java/pthiagodev/universidadeweb/api/domain/academico/curso/CursoRepository.java
@@ -0,0 +1,6 @@
+package pthiagodev.universidadeweb.api.domain.academico.curso;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CursoRepository extends JpaRepository<Curso, Long> {
+}
