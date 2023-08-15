cb9bdee0fad0fd6f5b97cdf18cfc41b0afc2cd95
Thiago de Assis <pthiagodev@gmail.com>
2023-08-14 21:50:39 -0300

    implementa controller de Curso

---
+++ src/main/java/pthiagodev/universidadeweb/api/controller/CursoController.java
@@ -0,0 +1,33 @@
+package pthiagodev.universidadeweb.api.controller;
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+import pthiagodev.universidadeweb.api.domain.academico.curso.Curso;
+import pthiagodev.universidadeweb.api.domain.academico.curso.CursoRepository;
+import pthiagodev.universidadeweb.api.domain.academico.curso.CursoRequest;
+import pthiagodev.universidadeweb.api.domain.academico.curso.CursoResponse;
+
+@RestController
+@RequestMapping("/cursos")
+public class CursoController {
+
+    @Autowired
+    private CursoRepository repository;
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity cadastrar(@RequestBody CursoRequest dados, UriComponentsBuilder uriBuilder) {
+        var curso = new Curso(dados);
+        repository.save(curso);
+
+        var uri = uriBuilder.path("/cursos/{id}").buildAndExpand(curso.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new CursoResponse(curso));
+    }
+}
